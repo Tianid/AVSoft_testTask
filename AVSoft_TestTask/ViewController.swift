949deda7e11370edeb536c43bea20af7a2fc8399
Tianid <tianid73@gmail.com>
2019-11-13 19:00:04 +0300

    Initial Commit

---
+++ AVSoft_TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVSoft_TestTask
+//
+//  Created by Tianid on 13.11.2019.
+//  Copyright © 2019 Tianid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
